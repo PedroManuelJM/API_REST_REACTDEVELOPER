f85fff81a468ad5a52df17965ab89d58555df54a
Pedro Manuel Jurado Moreno <mpedrojurado@gmail.com>
2021-01-10 18:06:17 -0500

    desplegando proyectp

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ render(){
   return (
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
   <div className="container">
-  <a className="navbar-brand" href="/">API REST - PRODUCTOS </a>
+  <a className="navbar-brand" href="/"> - API REST - PRODUCTOS -</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
